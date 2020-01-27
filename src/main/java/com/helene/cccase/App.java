5981cbafff7c1e2570a9f704afe09b451e7c9622
Helene Harmens <helene.harmens@gmail.com>
2020-01-28 00:00:08 +0100

    Initial commit

---
+++ src/main/java/com/helene/cccase/App.java
@@ -0,0 +1,16 @@
+package com.helene.cccase;
+
+/**
+ * CC CASE
+ * @author Helene Harmens
+ * 28.01.2020
+ */
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+
+}
